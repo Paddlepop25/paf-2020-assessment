c6527a2bdb50a59c01d50904b9409523b13c5875
Paddlepop25 <shecodeshsucodes18@gmail.com>
2020-12-18 12:50:35 +0800

    get formdata

--- frontend/src/app/authentication.service.ts
+++ frontend/src/app/authentication.service.ts
@@ -7,6 +7,9 @@ import { Login } from "./models";
 @Injectable()
 export class AuthenticationService {
 
+  username = ''
+  password = ''
+
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
@@ -15,11 +18,23 @@ export class AuthenticationService {
 
   async authenticateLogin(login: Login): Promise<any> {
     console.log(login)
+    // console.log(login['username'])
+    this.username = login['username'] // can log
+    this.password = login['password']
+
     return await this.http.post('http://localhost:3000', login, this.httpOptions)
     .toPromise()
     .catch((error: HttpErrorResponse) => {
       console.log('HttpError ---> ', error)
     })
   }
+
+  getUserNameAndPassword() {
+    return [this.username, this.password]
+  }
 }
 
+// result >>>  TextRow {
+//   user_id: 'fred',
+//   password: '31017a722665e4afce586950f42944a6d331dabf'
+// }
--- frontend/src/app/components/main.component.ts
+++ frontend/src/app/components/main.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from '../authentication.service';
 import {CameraService} from '../camera.service';
+import { CameraImage } from '../models';
 
 @Component({
   selector: 'app-main',
@@ -10,16 +13,48 @@ export class MainComponent implements OnInit {
 
 	imagePath = '/assets/cactus.png'
 
-	constructor(private cameraSvc: CameraService) { }
+	form: FormGroup
+	imageData
+
+	constructor(private fb: FormBuilder, private cameraSvc: CameraService, private authenticateSvc: AuthenticationService) { }
+
 
 	ngOnInit(): void {
 	  if (this.cameraSvc.hasImage()) {
-		  const img = this.cameraSvc.getImage()
+			const img = this.cameraSvc.getImage()
+			this.imageData = img.imageData
 		  this.imagePath = img.imageAsDataUrl
-	  }
+		}
+		
+		this.form = this.fb.group({
+      title: this.fb.control('', [ Validators.required ]),
+      comments: this.fb.control('', [ Validators.required ]),
+		})
+		
 	}
 
 	clear() {
 		this.imagePath = '/assets/cactus.png'
+	}
+
+	share() {
+		// get userName and password from service
+		let data = this.authenticateSvc.getUserNameAndPassword()
+		// console.log('username from main page >>> ', data[0]) // can log
+
+		let username = data[0]
+		let password = data[1]
+		// console.log(username) // can log
+		// console.log(password) // can log
+
+		let formData = new FormData();
+		formData.set('title', this.form.get('title').value)
+		formData.set('comments', this.form.get('comments').value)
+		formData.set('username', username)
+		formData.set('password', password)
+		formData.set('image', this.imageData)
+		
+		console.log(formData.get('image')) // can log
+		console.log('formData >>> ', formData)
 	}
 }
